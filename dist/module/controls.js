23e7297bbdf2bede6ae8d3116220d39a77de9140
Nehorim <pierrick.chouzenoux@gmail.com>
2021-05-07 23:51:28 -0400

    Commit dist folder

---
+++ dist/module/controls.js
@@ -0,0 +1,37 @@
+async function renderEntry(id) {
+    const cheatsheetPack = game.packs.get('dnd5-cheatsheet.dnd5-cheatsheet');
+    const cheatsheetEntry = (await cheatsheetPack.getEntity(id));
+    cheatsheetEntry.sheet.render(true);
+}
+Hooks.on('getSceneControlButtons', (controls) => {
+    controls.push({
+        name: 'cheatsheet',
+        title: 'dnd5Cheatsheet.controls.main',
+        icon: 'fas fa-dungeon',
+        layer: 'ControlsLayer',
+        visible: game.user.can('DRAWING_CREATE') || game.user.isGM,
+        tools: [
+            {
+                name: 'combat',
+                title: 'dnd5Cheatsheet.controls.combat',
+                icon: 'fas fa-fist-raised',
+                button: true,
+                onClick: () => renderEntry('96yLLrkGd9Wdgrh4'),
+            },
+            {
+                name: 'range-combat',
+                title: 'dnd5Cheatsheet.controls.range-combat',
+                icon: 'far fa-dot-circle',
+                button: true,
+                onClick: () => renderEntry('sWPEV6eJ09WE5hkZ'),
+            },
+            {
+                name: 'move',
+                title: 'dnd5Cheatsheet.controls.move',
+                icon: 'fas fa-running',
+                button: true,
+                onClick: () => renderEntry('nXtpaIsrorRtpVLh'),
+            },
+        ],
+    });
+});
